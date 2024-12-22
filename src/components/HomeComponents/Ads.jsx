1a4a03aa49b8cfab66abf1c05f1b89ba32020d87
syedhaamed <syedhaamed500@gmail.com>
2024-12-22 11:16:12 +0530

    typo mistake

--- src/components/HomeComponents/Ads.jsx
+++ src/components/HomeComponents/Ads.jsx
@@ -4,7 +4,7 @@ import Marquee from 'react-fast-marquee';
 
 function Ads() {
     return (
-        <div className='w-full h-auto my-10 py-2'><Marquee direction='left' speed={70} className='text-sm text-gray-600 font-cg-times font-bold sm:text-2xl md:text-3xl lg:text-4xl xl:tracking-widest'><span>MENTORSHIP FROM ARCHIEVERS </span> <span className='hidden md:inline ml-20'> MENTORSHIP FROM ARCHIEVERS</span></Marquee></div>
+        <div className='w-full h-auto my-10 py-2'><Marquee direction='left' speed={70} className='text-sm text-gray-600 font-cg-times font-bold sm:text-2xl md:text-3xl lg:text-4xl xl:tracking-widest'><span>MENTORSHIP FROM ACHIEVERS </span> <span className='hidden md:inline ml-20'> MENTORSHIP FROM ACHIEVERS</span></Marquee></div>
     )
 }
 
